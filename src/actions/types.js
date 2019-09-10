c13668c5a8b945858dc09b7455b9b414cdccd0a0
sultan820 <sultanbutt820@gmail.com>
2019-09-10 16:27:19 +0500

    upload on github

---
+++ src/actions/types.js
@@ -0,0 +1,4 @@
+export const INCREMENT = "INCREMENT";
+export const DECREMENT = "DECREMENT";
+export const CLEAR = "CLEAR";
+export const SET = "SET";
---
+++ src/components/Home.js
@@ -0,0 +1,83 @@
+import React, { Component } from "react";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button
+} from "react-native";
+import { connect } from "react-redux";
+import {
+  counterIncrement,
+  counterDecrement,
+  clearCounter,
+  counterSet
+} from "../actions";
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeText = this.onChangeText.bind(this);
+  }
+  onChangeText(number) {
+    const count = parseInt(number);
+    this.props.counterSet(number);
+  }
+
+  render() {
+    console.log(this.props);
+    const { container, countViewStyle, welcome } = styles;
+    return (
+      <View style={container}>
+        <TextInput
+          style={{
+            width: 40,
+            height: 40,
+            borderWidth: 1,
+            textAlign: "center"
+          }}
+          onChangeText={this.onChangeText}
+          value={this.props.count.toString()}
+        />
+        <View style={countViewStyle}>
+          <Button onPress={this.props.counterDecrement} title="-" />
+          <Text style={welcome}>{this.props.count}</Text>
+          <Button onPress={this.props.counterIncrement} title="+" />
+        </View>
+        <Button onPress={this.props.clearCounter} title="Clear" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 20
+  },
+  instructions: {
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  },
+  countViewStyle: {
+    flexDirection: "row"
+  }
+});
+
+function mapStateToProps(state) {
+  return { count: state };
+}
+
+export default connect(
+  mapStateToProps,
+  { counterIncrement, counterDecrement, clearCounter, counterSet }
+)(Home);
